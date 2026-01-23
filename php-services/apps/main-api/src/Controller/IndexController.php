8dfa7a094e4053538d1f14949cead024c6f39fac
twaydev <tungmv7@gmail.com>
2026-01-23 12:49:43 +0700

    ci: test code changes from php services

---
+++ php-services/apps/main-api/src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController
+{
+    #[Route("/", name: "index", methods: ["GET"])]
+    public function index(): JsonResponse
+    {
+        return new JsonResponse([
+            "message" => "Hello from PHP API Service",
+            "path" => $_SERVER["REQUEST_URI"],
+        ]);
+    }
+}
